8dba92bec453b39deda4dea8dd752cbe13f48e58
Felipe Trindade <felipe_biel3@hotmail.com>
2026-05-29 20:22:38 -0300

    fix: iniciando configurações do projeto
    Adicionando configurações básicas do projeto. Servidor, package.json e dependencias

---
+++ app.js
@@ -0,0 +1,23 @@
+//importando dependencias
+const express = require("express");
+const app  = express();
+const port = 3000;
+//configuração dos servidor para poder utilizar o EJS como view engine
+app.set('view engine', 'ejs');
+//configuração opcional - informando ao express aonde ele deve procurar as views
+app.set('views', './src/views');
+
+
+app.get('/', (req,res) => {
+    res.send("<h1>Servidor no ar!</h1>");
+})
+
+app.get('/teste', (req,res) => {
+    const TITULO = 'HelpDesk de TI';
+    res.render('index', {titulo: TITULO});
+})
+
+//inicializando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
